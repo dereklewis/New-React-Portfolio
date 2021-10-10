6b55d5ea7758a7f241b756fe4b0d81476ab71f4a
Derek Lewis <derek.lewis.den@outlook.com>
2021-10-10 10:40:28 -0600

    contact form updated and styled

--- client/src/Pages/Portfolio.js
+++ client/src/Pages/Portfolio.js
@@ -1,20 +1,11 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
 import YourImage from "../assets/IMG3466.png";
 
 const Portfolio = () => {
   return (
     <>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={YourImage} />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      <h1>Card placeholder</h1>
     </>
   );
 };
