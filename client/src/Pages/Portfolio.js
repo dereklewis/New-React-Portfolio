95d08be20f857338d43a88faa7b4d306f6eb1bb7
Derek Lewis <derek.lewis.den@outlook.com>
2021-10-10 16:47:39 -0600

    external link for portfolio cards complete

--- client/src/Pages/Portfolio.js
+++ client/src/Pages/Portfolio.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import { ExternalLink } from "react-external-link";
+import Concert from "../components/assets/concert.png";
 import YourImage from "../components/assets/IMG3466.png";
 
 const Project = () => {
@@ -10,12 +12,13 @@ const Project = () => {
           {/* <div className="flex-d flex-wrap justify-content-center">
             <div> */}
           <Card style={{ width: "18rem" }}>
-            <Card.Img variant="top" src={YourImage} />
+            <Card.Img variant="top" src={Concert} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>Concert Carpool</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                <ExternalLink href="https://github.com/dereklewis/New-React-Portfolio">
+                  <span>Github Repository</span>
+                </ExternalLink>
               </Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
