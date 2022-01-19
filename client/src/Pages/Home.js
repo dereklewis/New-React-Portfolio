9c206e5c534ebfa6e6d07c8e470446ec544ec1f7
Derek Lewis <derek.lewis.den@outlook.com>
2022-01-19 12:22:48 -0700

    home page reformatted to make it responsive

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,34 +1,35 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
 import YourImage from "../components/assets/fullbody.jpeg";
-import CloudImage from "../components/assets/cloudimage.jpg";
 
 const Home = () => {
   return (
     <Container
-      style={{
-        display: "Flex",
-        backgroundImage: "url(${ CloudImage })",
-      }}
+      className="homeContainer"
+      // style={{
+      //   display: "Flex",
+      //   backgroundImage: "url(${ CloudImage })",
+      // }}
     >
-      <Row xs={4} classname="image-row">
-        <div classname="image-div">
-          <img className="image" src={YourImage} fluid />
-        </div>
-      </Row>
-      <Row xs={8} classname="text-row">
-        {/* <image classname="cloud-image" src={CloudImage} fluid /> */}
-        <p className="text">
-          I am a Full Stack Web Developer with a Web Development Certificate
-          from the University of Denver. Experienced at building full stack web
-          applications from the ground up utilizing the MERN stack and other
-          frameworks/libraries and databases. Adept at working within diverse
-          groups of all sizes. Strong interpersonal skills and a team player
-          that is passionate about the success of the organization. Experience
-          with large complex projects with multiple stakeholders such as new
-          restaurant/hotel openings and transitions.
-        </p>
-      </Row>
+      {/* <Row xs={4} classname="image-row"> */}
+      <div classname="image-div">
+        <img className="image" src={YourImage} fluid />
+      </div>
+      {/* </Row> */}
+      {/* <Row xs={8} classname="text-row"> */}
+      {/* <image classname="cloud-image" src={CloudImage} fluid /> */}
+      <div className="textDiv">
+        {/* <p className="text"> */}I am a Full Stack Web Developer with a Web
+        Development Certificate from the University of Denver. Experienced at
+        building full stack web applications from the ground up utilizing the
+        MERN stack and other frameworks/libraries and databases. Adept at
+        working within diverse groups of all sizes. Strong interpersonal skills
+        and a team player that is passionate about the success of the
+        organization. Experience with large complex projects with multiple
+        stakeholders such as new restaurant/hotel openings and transitions.
+        {/* </p> */}
+      </div>
+      {/* </Row> */}
     </Container>
   );
 };
